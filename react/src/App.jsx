d90c5e897b5cd99a1720af575a7509360a832354
Johnsmart <johnsmart170899@gmail.com>
2024-03-22 22:20:39 +0100

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import  { useState } from 'react';
+import './App.css';
+
+function App() {
+    
+      return (
+        <div className="App">
+          <header>
+      <h1>Johnsmart's To-do list</h1>
+    </header>
+    </div>
---
+++ react/to-do.js
@@ -0,0 +1,59 @@
+// TodoList.js
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleToggleComplete = (id) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    }));
+  };
+
+  return (
+    <div className="todo-list">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Add a new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onDelete={handleDelete}
+            onToggleComplete={handleToggleComplete}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
